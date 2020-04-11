c1a97bd45c7469f4d7f2329861a363affc687443
yanyuan <yanyuan@yanyuandeMacBook-Pro.local>
2020-04-11 11:06:28 +0800

    CEO审批5天及以下

--- src/main/java/com/yanyuan/gof/behavior/chain/CEOLeader.java
+++ src/main/java/com/yanyuan/gof/behavior/chain/CEOLeader.java
@@ -4,28 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Description ConcreteHandler实现类
- * 经理 批准3天及以下
+ * CEO 批准5天及以下
  * 职责链上的一个具体处理者
  * @Author yanyuan
  * @Date 23:58 2020/4/10
  * @Version 1.0
  **/
 @Slf4j
-public class ManageLeader extends Leader{
+public class CEOLeader extends Leader{
 
-    public ManageLeader(String position) {
+    public CEOLeader(String position) {
         super(position);
     }
 
     @Override
     public void process(LeaveRequest leaveRequest) {
-        if(leaveRequest.getDays() <= 3){
+        if(leaveRequest.getDays() <= 5){
             log.info("请假单：姓名{}, 请假天数: {} , 原因：{}",
                     leaveRequest.getName(),
                     leaveRequest.getDays() ,
                     leaveRequest.getReason());
-            log.info("经理批准: 同意");
-        }else if(leaveRequest.getDays() > 3){
+            log.info("CEO批准: 同意");
+        }else if(leaveRequest.getDays() > 5){
             if(nextLeader == null){
                 log.info("该请假单不能被批准：{} ", leaveRequest);
                 return;
--- src/main/java/com/yanyuan/gof/behavior/chain/Test.java
+++ src/main/java/com/yanyuan/gof/behavior/chain/Test.java
@@ -10,16 +10,20 @@ public class Test {
     public static void main(String[] args){
         Leader groupLeader = new GroupLeader("组长");
         Leader manageLeader = new ManageLeader("经理");
-
-        LeaveRequest oneDay = new LeaveRequest("林冲", 1, "爬山");
         groupLeader.setNextLeader(manageLeader);
 
+        Leader ceo = new CEOLeader("总经理");
+        manageLeader.setNextLeader(ceo);
+
+        LeaveRequest oneDay = new LeaveRequest("林冲", 1, "爬山");
 
         LeaveRequest threeDays = new LeaveRequest("吴用", 3, "回家相亲");
-        LeaveRequest fiveDays = new LeaveRequest("李逵", 5, "去打猎");
+        LeaveRequest fiveDays = new LeaveRequest("武松", 5, "去打猎");
+        LeaveRequest sixDays = new LeaveRequest("李逵", 7, "去旅行");
 
         groupLeader.process(oneDay);
         groupLeader.process(threeDays);
         groupLeader.process(fiveDays);
+        groupLeader.process(sixDays);
     }
 }
