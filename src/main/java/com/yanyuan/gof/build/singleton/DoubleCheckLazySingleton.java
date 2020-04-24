22f7111d8f4590f0513e72803da99a1d9e7f7808
yanyuan <yanyuan@yanyuandeMacBook-Pro.local>
2020-04-24 13:33:35 +0800

    单例模式

--- src/main/java/com/yanyuan/gof/build/singleton/DoubleCheckLazySingleton.java
+++ src/main/java/com/yanyuan/gof/build/singleton/DoubleCheckLazySingleton.java
@@ -7,12 +7,17 @@ package com.yanyuan.gof.build.singleton;
  * @Version 1.0
  **/
 public class DoubleCheckLazySingleton {
+    /**
+     * volatile修饰变量：
+     * 1、保证了不同线程对这个变量进行操作时的可见性，即一个线程修改了某个变量的值，这新值对其他线程来说是立即可见的。
+     * 2、禁止进行指令重排序。
+     */
     private static volatile DoubleCheckLazySingleton doubleCheckLazySingleton;
     private DoubleCheckLazySingleton(){
     }
     public static DoubleCheckLazySingleton getInstance(){
-        if(doubleCheckLazySingleton == null){
-            synchronized(DoubleCheckLazySingleton.class){
+        if(doubleCheckLazySingleton == null){//第一次检查
+            synchronized(DoubleCheckLazySingleton.class){//第二次检查
                 if(doubleCheckLazySingleton == null){
                     doubleCheckLazySingleton = new DoubleCheckLazySingleton();
                 }
